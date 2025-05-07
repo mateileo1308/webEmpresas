f77d78ebf712895d082a0caec2061cb3e083d777
matei1308 <leomatamoros7@yahoo.es>
2025-05-07 12:10:33 +0200

    Hemos introducido el MIXiN, herramienta para evitar vulnerabilidades

--- pages/views.py
+++ pages/views.py
@@ -3,23 +3,34 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Page
 from django.urls import reverse, reverse_lazy
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import redirect, render, get_object_or_404, get_list_or_404
 from .forms import PageForm
+
+
+class StaffRequiredMixin(object):
+    """
+    Este Mixin requiere que el usuario sea parte del staff
+    """
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
 
 # Create your views here.
 class pagesListView(ListView):
     model = Page
+    #user_ = request.user
+    #print(f"en la vista principal {user_}")
 
 class pageDetailView(DetailView):
     model = Page
 
-class pageCreateView(CreateView):
+class pageCreateView(StaffRequiredMixin, CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
-
-class pageUpdateView(UpdateView):
+class pageUpdateView(StaffRequiredMixin, UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = "_update_form"
@@ -27,28 +38,9 @@ class pageUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args = [self.object.id]) + '?ok'
 
-class pageDeleteView(DeleteView):
+class pageDeleteView(StaffRequiredMixin, DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
 
 
 
-
-
-"""
-    title = models.CharField(verbose_name="Título", max_length=200)
-    content = RichTextField(verbose_name="Contenido")
-    order = models.SmallIntegerField(verbose_name="Orden", default=0)
-    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
-    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
-    pages = get_list_or_404(Page)
-    return render(request, 'pages/pages.html', {'pages':pages})
-
-def page(request, page_id, page_slug):
-    page = get_object_or_404(Page, id=page_id)
-    return render(request, 'pages/page.html', {'page':page})
-
-def get_success_url(self):
-        return reverse('pages:pages')
-
-"""
