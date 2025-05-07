b5defc5bd284105737f0db11ae60d6a30d019476
matei1308 <leomatamoros7@yahoo.es>
2025-05-07 13:07:03 +0200

    Hemos creado una nueva app: registration. que sirve para registrar los usuarios

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), #path de auth
 
 ]
